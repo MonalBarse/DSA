e414ba45e50391a58bfc944191e0e1e57297fe33
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-06-28 12:27:19 +0530

    More QnA

--- BasicMaths/src/com/monal/BitManipulation.java
+++ BasicMaths/src/com/monal/BitManipulation.java
@@ -2,8 +2,106 @@ package com.monal;
 
 public class BitManipulation {
 
-  public static void main(String[] args) {
-    System.out.println(findMagicNumber(3)); // 30
+  public static void main(String[] args) {}
+
+  // -------------------------------------------------------------------- //
+
+  public static double sqrtNR(int number, int precision) {
+    double x = number; // Initial guess for the square root
+    double root; // Variable to store the calculated square root
+    double threshold =
+        Math.pow(10, -precision); // Precision threshold based on the desired precision
+
+    while (true) {
+      root = 0.5 * (x + (number / x)); // Update the guess
+
+      if (Math.abs(root - x) < threshold) {
+        break; // If the difference is within the precision threshold, stop the iteration
+      }
+
+      x = root;
+    }
+
+    return root;
+  }
+
+  // -------------------------------------------------------------------- //
+
+  public static double sqrtBS(int number, int precision) {
+    int s = 0;
+    int e = number;
+    double root = 0;
+
+    // Binary search for the integer part
+    while (s <= e) {
+      int mid = s + (e - s) / 2;
+      if (mid * mid == number) {
+        return mid;
+      } else if (mid * mid < number) {
+        root = mid;
+        s = mid + 1;
+      } else {
+        e = mid - 1;
+      }
+    }
+
+    // Linear search for the fractional part
+    double increment = 0.1;
+    for (int i = 0; i < precision; i++) {
+      while (root * root <= number) {
+        root += increment;
+      }
+
+      // we also need to round off the number to the given precision
+      // we can do this by multiplying the number by 10^precision, rounding it off and then dividing
+      // it by 10^precision
+
+      root = root - increment;
+      increment = increment / 10;
+    }
+    root = Math.round(root * Math.pow(10, precision)) / Math.pow(10, precision);
+    return root;
+    /*
+     * int number = 50;
+     * int precision = 3;
+     * double sqrt = sqrtBS(number, precision);
+     * System.out.println("Square root of " + number + " with precision " + precision + " is: " + sqrt);
+     */
+  }
+
+  // -------------------------------------------------------------------- //
+
+  public static void seive(int number) {
+    // Given a number find all the prime numbers less than or equal to it
+    // We can solve this problem using Seive of Eratosthenes
+    // The algorithm works by marking the multiples of each prime number starting from 2 as
+    // composites
+    // and then finding the next prime number and repeating the process
+    // The time complexity of this algorithm is O(nlog(logn)) and space complexity is O(n)
+    // The algorithm is efficient for finding all prime numbers up to 10 million and not efficient
+    // for finding in a range.
+
+    // We will mark the prime numbers as true and the non prime numbers as false
+    boolean[] isPrime =
+        new boolean[number + 1]; // Initialzing an empty array puts all the values to false
+    for (int i = 2; i <= number; i++) {
+      isPrime[i] = true;
+    }
+    // We are marking all the numbers as true as we are assuming all the numbers to be prime
+    // and the prime numbers as true
+    for (int i = 2; i * i <= number; i++) {
+      if (isPrime[i]) {
+        for (int j = i * 2; j <= number; j += i) {
+          isPrime[j] = false;
+        }
+      }
+    }
+    for (int i = 2; i <= number; i++) {
+      if (isPrime[i]) {
+        System.out.print(i + " ");
+      }
+    }
+    // Calculating the complexity of the algorithm
   }
 
   // -------------------------------------------------------------------- //
@@ -24,6 +122,8 @@ public class BitManipulation {
       // right and get last bit of the number
     }
     return answer;
+
+    // System.out.println(findMagicNumber(3)); // 30
   }
 
   // -------------------------------------------------------------------- //
