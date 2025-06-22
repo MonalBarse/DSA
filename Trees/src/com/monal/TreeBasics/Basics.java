70869d2e4929731ecf969b1cba3dfdf850b9363c
Monal Barse <Monal@MacBook-Air.local>
2025-06-22 16:39:09 +0530

    Started Trees

--- Trees/src/com/monal/TreeBasics/Basics.java
+++ Trees/src/com/monal/TreeBasics/Basics.java
@@ -1,7 +1,7 @@
 package com.monal.TreeBasics;
 
 public class Basics {
-    public static void main(String[] args) {
-        System.out.println("Hello from Basics!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello from Basics!");
+  }
 }
